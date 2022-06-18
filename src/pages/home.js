0eb670d3364f4a9710244fcebe9864ef99c3782b
mmergal92 <79428668+mmergal92@users.noreply.github.com>
2022-06-18 14:20:18 -0400

    updates to layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Video from '../components/video'
 import Spanish from './spanish'
 import Rotation from '../components/rotation'
-import circletext_logo from '../assets/circletext_logo.png'
+import circletext_nologo from '../assets/circletext_nologo.png'
 import pexels_mart_production from "../assets/pexels_mart_production.mp4";
 
 const Home = () =>{
@@ -11,12 +11,16 @@ const Home = () =>{
         <div className= 'home'>
               <Video />
               <div className = "middle-container">
-                <a href="mailto:maria@itsproof.co" target="_blank" rel="noreferrer noopener" ><img src={circletext_logo}  /></a>
+                <a href="mailto:maria@itsproof.co" target="_blank" rel="noreferrer noopener" ><img src={circletext_nologo} /><h1>P</h1> </a>
               </div>
               <div className = "right-container">
                   <div className ="home-text">
-                    <h2>Proof is a creative agency crafting innovative and inclusive experiences for the digital world.</h2> <br/>
-                    <p>Proof was cofounded by <span className ="link"><a href="https://www.segacyroberts.com/" target="_blank" rel="noreferrer noopener" >Segacy</a></span> and <span className="link"><a href="https://mariamergal.dev/" target="_blank" rel="noreferrer noopener" >Maria,</a></span> two first-gen New Yorkers and childhood friends, to amplify the stories of women of color and transform the world through design and technology. We work with entrepreneurs and businesses of all sizes to tell their stories through <span className="italics_p">strategy, design,</span> and <span className="italics_p">development.</span></p>
+                    <h2>pr<i>oo</i>f</h2>
+                    <br/>
+                    <br/>
+                    <p>proof is a creative agency crafting innovative and inclusive experiences for the digital world.</p> <br/>
+                    <br/>
+                    <p>proof was cofounded by <span className ="link"><a href="https://www.segacyroberts.com/" target="_blank" rel="noreferrer noopener" >Segacy</a></span> and <span className="link"><a href="https://mariamergal.dev/" target="_blank" rel="noreferrer noopener" >Maria,</a></span> two first-gen New Yorkers and childhood friends, to amplify the stories of women of color and transform the world through design and technology. We work with entrepreneurs and businesses of all sizes to tell their stories through <span className="italics_p">strategy, design,</span> and <span className="italics_p">development.</span></p>
                     <br/>
                     <br/>
                     <p><Link to="/spanish">en español</Link></p>
