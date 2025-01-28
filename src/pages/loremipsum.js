f41be9193fe78f52b190687987d817d2659683df
Maria Mergal <maria@curedesign.co>
2025-01-28 17:52:10 -0500

    editing lorem

--- src/pages/loremipsum.js
+++ src/pages/loremipsum.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { loremText } from '../components/loremtext';
 import Home from './home';
 import Video from '../components/video'
@@ -6,26 +6,26 @@ import { Link } from 'react-router-dom';
 import pexels_mart_production from "../assets/pexels_mart_production.mp4";
 import circletext_nologo from "../assets/circletext_nologo.png"
 
-class Loremipsum extends Component {
-    state = {
-      showCopyModal: false,
-      numberOfParagraphs: '',
-      text: '',
+
+const Loremipsum = () => {
+    const [showCopyModal, setShowCopyModal] = useState(false);
+    const [numberOfParagraphs, setNumberOfParagraphs] = useState('');
+    const [text, setText] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const number = parseFloat(numberOfParagraphs);
+      const HTMLParagraphs = loremText.getAllParagraphs(number);
+      setText(HTMLParagraphs);
     };
   
-    handleSubmit = e => {
-      e.preventDefault();
-      const numberOfParagraphs = parseFloat(this.state.numberOfParagraphs);
-      const HTMLParagraphs = loremText.getAllParagraphs(numberOfParagraphs);
-      this.setState({ text: HTMLParagraphs });
-    };
-    handleCopy = () => {
+    const handleCopy = () => {
       const el = document.createElement('textarea'); // Create a <textarea> element
   
-      // Convert html string to text to remove the <p></p> elements
+      // Convert HTML string to text to remove the <p></p> elements
       const tmp = document.createElement('DIV');
-      tmp.innerHTML = this.state.text;
-      el.innerHTML = tmp.textContent || tmp.innerText || ''; // Set the <textarea> value to the string that you want copied
+      tmp.innerHTML = text;
+      el.innerHTML = tmp.textContent || tmp.innerText || ''; // Set the <textarea> value to the string to be copied
   
       el.setAttribute('readonly', ''); // Make it readonly to be tamper-proof
       el.style.position = 'absolute';
@@ -38,15 +38,18 @@ class Loremipsum extends Component {
       el.select(); // Select the <textarea> content
       document.execCommand('copy'); // Copy - only works as a result of a user action (e.g. click events)
       document.body.removeChild(el); // Remove the <textarea> element
+  
       if (selected) {
         // If a selection existed before copying
         document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
         document.getSelection().addRange(selected); // Restore the original selection
       }
-      this.setState({ showCopyModal: true });
-      setTimeout(() => this.setState({ showCopyModal: false }), 1000);
+  
+      setShowCopyModal(true);
+      setTimeout(() => setShowCopyModal(false), 1000);
     };
-    render() {
+
+
       return (
         <div>
   
@@ -62,38 +65,36 @@ class Loremipsum extends Component {
                 type="number"
                 className="paragraph-number"
                 name="numberOfParagraphs"
-                onChange={e => this.setState({ numberOfParagraphs: e.target.value })}
-                value={this.state.numberOfParagraphs}
+                onChange={(e) => setNumberOfParagraphs(e.target.value)}
+                value={numberOfParagraphs}
               />
               <input type="submit" value="Generate" className="generate-button" />
             </form>
-            {this.state.text && (
-              <>
-                <a className="copy-button" onClick={this.handleCopy}>
-                  Copy Text
-                </a>
-                <div
-                  id="targetText"
-                  className="generated-text"
-                  dangerouslySetInnerHTML={{ __html: this.state.text }}
-                  ref={ref => (this.text = ref)}
-                />
-              </>
-            )}
+            {text && (
+          <>
+            <button className="copy-button" onClick={handleCopy}>
+              Copy Text
+            </button>
+            <div
+              id="targetText"
+              className="generated-text"
+              dangerouslySetInnerHTML={{ __html: text }}
+            />
+          </>
+        )}
           </div>
   
-          <div
-            id="copyOverlay"
-            className={`${this.state.showCopyModal ? 'modal--show' : 'modal--hidden'}`}
-          >
+          <div id="copyOverlay" className={`${showCopyModal ? 'modal--show' : 'modal--hidden'}`}>
+
             <div id="copyText">
               <p>Copied!</p>
             </div>
           </div>
         </div>
       );
-    }
-  }
+
+}
+
   
   export default Loremipsum;
 
