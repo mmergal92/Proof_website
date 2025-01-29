0bfa4a7b5181bbfda8ccf9e0b8de236c9e22d0bc
Maria Mergal <maria@curedesign.co>
2025-01-29 13:39:41 -0500

    editing text

--- src/components/loremtext.js
+++ src/components/loremtext.js
@@ -8,21 +8,19 @@ function GenerateNewText() {
   // Add property to the object
   this.sentences =
     [
-      'Piece of ginger and thyme don’t forget pimento seeds. I mix everything together. Grace and Maggi of course. Now we’re going to wash the chicken. Do what you please in your house but as for us in this house? As for Deddy and fi him house? Lawd ah mercy, tek back yuhself and bring back yuhself inna one peace. She’s as good as gold and scotch bonnet peppers know who fi frighten.',
-      'Whatever I said whatever I did I didn’t mean it scotchies highgate saint mary. Small up yuhself! Edna Leroy and Everton remember the songs used to make you rock away. Tastee or juici or golden krust jerk ribs and rice and peas where is the oxtail gravy. We have food at home.',
-      'testing thisThis is not yours to fix alone. You act like you’re all alone out there in the world, but you’re not. You’re not alone.',
-      'testing thisWhy are you keeping this curiosity door lock?',
-      'testing thisDo you know anything about sensory deprivation tanks? Specifically how to build one?',
-      'testing thisIf anyone asks where I am, I\'ve left the country.',
-      'testing thisYOU BETTER RUN! She\'s our friend, and she\'s crazy!',
-      'testing thisMouth-breather.',
-      'testing thisNancy, seriously, you\'re gonna be so cool now, it\'s ridiculous.',
-      'testing thisUm, I\'m happy you\'re home.',
-      'testing thisYou’re going to take out the demigorgon with a slingshot?',
-      'testing thisMornings are for coffee and contemplation.',
-      'testing thisYou shouldn\'t like things because people tell you you\'re supposed to.',
-      'testing thisNo... no El, you\'re not the monster. You saved me. Do you understand? You saved me.',
-      'testing thisWe never would\'ve upset you if we knew you had superpowers.',
+      'Piece of ginger and thyme don\'t forget pimento seeds.',
+      'Whatever I said whatever I did I didn\'t mean it scotchies highgate saint mary.',
+      'I mix everything together.',
+      ' Grace and Maggi of course.',
+      'Now we\'re going to wash the chicken.',
+      'Do what you please in your house but as for us in this house?',
+      'As for Deddy and fi him house?',
+      'Lawd ah mercy, tek back yuhself and bring back yuhself inna one peace.',
+      'She\'s as good as gold and scotch bonnet peppers know who fi frighten.',
+      'Small up yuhself! ',
+      'Edna Leroy and Everton remember the songs used to make you rock away.',
+      'Tastee or juici or golden krust jerk ribs and rice and peas where is the oxtail gravy.',
+      'We have food at home.',
    ];
 }
 
