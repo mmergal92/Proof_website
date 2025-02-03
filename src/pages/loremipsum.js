56eece65dd4ca0aca2ea32d46833c0f3dfcacc4f
Maria Mergal <maria@curedesign.co>
2025-02-03 12:00:57 -0500

    edit

--- src/pages/loremipsum.js
+++ src/pages/loremipsum.js
@@ -79,10 +79,10 @@ const Loremipsum = () => {
           <div className="main-content ">
             <div className="left-lorem-container" style={{ backgroundColor: bgColor, color: fontColor}}>
                 <div className ="language-button">
-                <button className={`plaintain-button ${language === 'english' ? 'active' : ''}`} onClick={() => setLanguage('english')}>
+                <button className={`plaintain-button ${language === 'english' ? 'active' : ''}`} onClick={() => setLanguage('english')} style={{ backgroundColor: bgColor, color: fontColor}}>
                       plantain
                   </button>
-                  <button className={`platano-button ${language === 'spanish' ? 'active' : ''}`} onClick={() => setLanguage('spanish')}>
+                  <button className={`platano-button ${language === 'spanish' ? 'active' : ''}`} onClick={() => setLanguage('spanish')} style={{ backgroundColor: fontColor, color: bgColor}}>
                       plátano
                   </button>
                 </div>
