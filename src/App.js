126f45cf4c57c5a05ec261ce30bd3d62408115dd
Maria Mergal <maria@curedesign.co>
2025-03-12 11:55:30 -0400

    editing app ga4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
 import './App.css';
 import './assets/VulfSansDemo-Regular.otf';
 //IMPORT ROUTE and COMPONENTS
@@ -10,6 +11,22 @@ import Loremipsum from './pages/loremipsum';
 // import Nav from "./components/Nav"
 // import {Redirect} from 'react-router';
 import Footer from "./components/footer";
+
+const MEASUREMENT_ID = 'G-4ZV4YCN0DD';
+ReactGA.initialize(MEASUREMENT_ID);
+
+import { useLocation } from 'react-router-dom';
+
+const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+};
+
+// In your component where routes are defined (e.g., App.js)
+usePageTracking();
 
 function App() {
   return (
