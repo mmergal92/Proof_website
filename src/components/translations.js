e71d6694b98014009d041c3a026bdc29eb2d8331
Maria Mergal <maria@curedesign.co>
2025-05-23 11:23:49 -0400

    edit

---
+++ src/components/translations.js
@@ -0,0 +1,24 @@
+const t = {
+    en: {
+      work: "Work",
+      about: "About",
+      email: "Email us",
+      headline: "Fresh and imaginative experiences for the internet and elsewhere.",
+      testimonials: "Testimonials",
+      rights: "2025. All Rights Reserved.",
+      lang: "En español",
+      lorem: "Riddim Ipsum",
+    },
+    es: {
+      work: "Trabajo",
+      about: "Acerca de",
+      email: "Contáctanos",
+      headline: "Experiencias frescas e imaginativas para el internet y más allá.",
+      testimonials: "Testimonios",
+      rights: "2025. Todos los derechos reservados.",
+      lang: "In english",
+      lorem: "Ritmo Ipsum",
+    }
+  };
+  
+  export default t;
