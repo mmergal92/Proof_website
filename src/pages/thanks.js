7ce6a65af95a0c0973be29d362d0a9a6bb563477
Maria Mergal <maria@curedesign.co>
2025-07-29 18:12:26 -0400

    edit

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ThankYou = () => {
   return (
     <div className="author-page-thanks">
-      <section className="author-hero">
-        <div className="hero-content">
+      <section className="author-hero-thanks">
+        <div className="hero-content-thanks">
           <h1>Thank you!</h1>
           <p>We’ve received your submission and will be in touch shortly to schedule your call. We can’t wait to help you build your dream website.</p>
           <a href="/authors" className="author-btn">Back to home</a>
