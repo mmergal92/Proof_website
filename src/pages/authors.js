d9c81bbc4ac786c7d7d929b445ef42f368029cce
Maria Mergal <maria@curedesign.co>
2025-07-30 15:03:46 -0400

    edit

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -9,8 +9,8 @@ import vector from '../assets/Vector.png'
 const Authors = () =>{
   return ( 
     <div className="author-page asection">
-      <section className="author-herop  "> 
-        <div className="hero-contentp ">
+      <section className="author-hero  "> 
+        <div className="hero-content ">
             <h5>Three fiction authors. Three stunning transformations. This summer only.</h5>
             <h1>An obsession-worthy website in just 6-weeks</h1>
             <a href="#offer" className="author-btn">See what's included</a>
