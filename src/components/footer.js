9fada987332070d14419700213e39a8d44228c2b
Maria Mergal <maria@curedesign.co>
2025-08-18 18:13:26 -0400

    edit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+import instagram from '../assets/instagram.png';
 
 const Footer = ({ language, t }) => (
   <footer className="site-footer">
     <div className="footer-container">
       <nav className="footer-left footer-links">
-        <a href="mailto:hello@itsproof.co">{t.email}</a>
-        <a href="http://instagram.com/itsproof.co" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="/islandipsum" target="_blank" rel="noopener noreferrer">{t.lorem}</a>
+        {/* <a href="mailto:hello@itsproof.co">{t.email}</a> */}
+        <a href="http://instagram.com/itsproof.co" target="_blank" rel="noopener noreferrer">
+          <img src={instagram} alt="Instagram" className="instagram-icon" />
+        </a>
+        {/* <a href="/islandipsum" target="_blank" rel="noopener noreferrer">{t.lorem}</a> */}
       </nav>
-      <div className="footer-logo"><Link to={`/?lang=${language}`}>PROOF</Link></div>
+      {/* <div className="footer-logo"><Link to={`/?lang=${language}`}>PROOF</Link></div> */}
       <nav className="footer-right footer-rights">
         <p className="footer-right-nav">{t.rights}</p>
       </nav>
